5c8412b008a72ec3a8bc0b94036dc4e89e7cbee7
A1Zab <lisandrugrin@gmail.com>
2023-06-27 12:41:16 +0300

    .

--- src/store/store.js
+++ src/store/store.js
@@ -12,7 +12,7 @@ function createStore() {
 	return configureStore({
 		reducer: rootReducer,
 		middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-		devTools: process.env.NODE_ENV !== 'production'
+		devTools: import.meta.env.DEV
 	})
 }
 
